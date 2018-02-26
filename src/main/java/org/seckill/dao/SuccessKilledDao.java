cfe2dc38aa0159e1c5b45810935b5e94229d4ae0
xunlingzi <codety@hotmail.com>
2018-02-27 00:13:15 +0800

    测试 debug

--- src/main/java/org/seckill/dao/SuccessKilledDao.java
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -1,21 +1,22 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.SuccessKilled;
 
 public interface SuccessKilledDao {
 
     /**
      * 插入购买明细，可过滤重复（联合主键）
-     * @param seckilId
+     * @param seckillId
      * @param userPhone
      * @return 插入的行数
      */
-    int insertSuccessKilled(long seckilId, long userPhone);
+    int insertSuccessKilled(@Param("seckillId") long seckillId,@Param("userPhone") long userPhone);
 
     /**
      * 根据id查询SuccessKilled并携带秒杀产品对象实体
      * @param seckillId
      * @return
      */
-    SuccessKilled queryByIdWithSeckill(long seckillId);
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
 }
--- src/test/java/org/seckill/dao/SeckillDaoTest.java
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -24,10 +27,6 @@ public class SeckillDaoTest {
     private SeckillDao seckillDao;
 
     @Test
-    public void reduceNumber() {
-    }
-
-    @Test
     public void queryById() {
         long id = 1000;
         Seckill seckill = seckillDao.queryById(id);
@@ -37,6 +36,20 @@ public class SeckillDaoTest {
 
     @Test
     public void queryAll() {
+        /**
+         * org.apache.ibatis.binding.BindingException: Invalid bound statement (not found): org.seckill.dao.SeckillDao.queryAll
+         * java没有保存形参的记录：queryAll(int offset, int limit) -> queryAll(arg0, arg1)
+         */
+        List<Seckill> seckills = seckillDao.queryAll(0, 100);
+        for (Seckill seckill : seckills) {
+            System.out.println(seckill);
+        }
+    }
 
+    @Test
+    public void reduceNumber() {
+        Date killTime = new Date();
+        int updateCount = seckillDao.reduceNumber(1000L, killTime);
+        System.out.println("updateTime = " + updateCount);
     }
 }
