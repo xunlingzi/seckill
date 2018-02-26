d021a775e1e80e7720f5ef886e9ca85c400dcb9f
xunlingzi <codety@hotmail.com>
2018-02-27 00:48:07 +0800

    Service接口设计

---
+++ src/main/java/org/seckill/service/SeckillService.java
@@ -0,0 +1,46 @@
+package org.seckill.service;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+
+import java.util.List;
+
+/**
+ * 业务接口：站在“使用者”角度设计接口（不要太关注实现细节）
+ * 三个方面：方法定义粒度（明确），参数（简练），返回类型（return 类型友好/异常）
+ */
+public interface SeckillService {
+
+    /**
+     * 查询所有秒杀记录
+     * @return
+     */
+    List<Seckill> getSeckillList();
+
+    /**
+     * 查询单个秒杀记录
+     * @param seckillId
+     * @return
+     */
+    Seckill getById(long seckillId);
+
+    /**
+     * 秒杀开启时输出秒杀接口地址
+     * 否则输出系统时间和秒杀时间
+     * @param seckillId
+     */
+    Exposer exportSeckillUrl(long seckillId);
+
+    /**
+     * 执行秒杀操作
+     * @param seckillId
+     * @param userPhone
+     * @param mdt5
+     */
+    SeckillExecution executeSeckill(long seckillId, long userPhone, String mdt5)
+        throws SeckillException, RepeatKillException, SeckillCloseException;
+}
